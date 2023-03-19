67e4e4838c473791847c74475c1197ef1dbb5309
Karsei <karseidev@gmail.com>
2023-03-19 18:22:25 +0900

    refact: DB 에 따라 번호 결정하도록 수정

--- application/src/main/java/kr/pe/karsei/blogsearch/adapter/out/BlogKeywordCollectJpaEntity.java
+++ application/src/main/java/kr/pe/karsei/blogsearch/adapter/out/BlogKeywordCollectJpaEntity.java
@@ -16,7 +16,7 @@ import java.util.Date;
 @EntityListeners(AuditingEntityListener.class)
 public class BlogKeywordCollectJpaEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue
     private Long id;
 
     @Column(unique = true)
