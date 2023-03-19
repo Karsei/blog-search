b422a44d0f16b895bc1c3a3199d874e25daf0b8e
Karsei <karseidev@gmail.com>
2023-03-19 23:10:53 +0900

    refact: 스케쥴러 설정 위치 변경

--- application/src/main/java/kr/pe/karsei/BlogSearchApplication.java
+++ application/src/main/java/kr/pe/karsei/BlogSearchApplication.java
@@ -3,10 +3,8 @@ package kr.pe.karsei;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableAsync
-@EnableScheduling
 @SpringBootApplication
 public class BlogSearchApplication {
     public static void main(String[] args) {
---
+++ application/src/main/java/kr/pe/karsei/blogsearch/config/SchedulerConfiguration.java
@@ -0,0 +1,11 @@
+package kr.pe.karsei.blogsearch.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+@ConditionalOnProperty(prefix = "scheduler", name = "enabled", havingValue = "true", matchIfMissing = true)
+public class SchedulerConfiguration {
+}
