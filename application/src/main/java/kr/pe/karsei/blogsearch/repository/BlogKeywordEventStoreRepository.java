1e3f58638ba8fffc0d6fb3ac8efd4b12fdb01e49
Karsei <karseidev@gmail.com>
2023-03-19 18:26:29 +0900

    refact: stream 으로 하나씩 조회하도록 수정

--- application/src/main/java/kr/pe/karsei/blogsearch/repository/BlogKeywordEventStoreRepository.java
+++ application/src/main/java/kr/pe/karsei/blogsearch/repository/BlogKeywordEventStoreRepository.java
@@ -3,8 +3,8 @@ package kr.pe.karsei.blogsearch.repository;
 import kr.pe.karsei.blogsearch.adapter.out.BlogKeywordEventStoreJpaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+import java.util.stream.Stream;
 
 public interface BlogKeywordEventStoreRepository extends JpaRepository<BlogKeywordEventStoreJpaEntity, Long> {
-    List<BlogKeywordEventStoreJpaEntity> findAllByAck(boolean ack);
+    Stream<BlogKeywordEventStoreJpaEntity> findAllByIdGreaterThanOrderByCreatedAtAsc(Long id);
 }
