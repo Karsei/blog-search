5458d8afe0908178d7412574d794aeb9eae0f220
Karsei <karseidev@gmail.com>
2023-03-19 18:47:12 +0900

    docs: 주석 삭제

--- client/kakao/src/main/java/kr/pe/karsei/client/kakao/config/KakaoClientConfiguration.java
+++ client/kakao/src/main/java/kr/pe/karsei/client/kakao/config/KakaoClientConfiguration.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Bean;
 public class KakaoClientConfiguration {
     @Bean
     public RequestInterceptor requestInterceptor() {
-        // TODO: url, key 를 property 로 빼내기
         return requestTemplate -> requestTemplate.header("Authorization", "KakaoAK " + "");
     }
 }
