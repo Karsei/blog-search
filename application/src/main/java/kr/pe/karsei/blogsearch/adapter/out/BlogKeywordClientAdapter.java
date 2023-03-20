26819769a679bdc095b08e3080e7994fd448ba54
Karsei <karseidev@gmail.com>
2023-03-20 18:00:48 +0900

    feat: client adapter 에 naver 클라이언트 추가

--- application/src/main/java/kr/pe/karsei/blogsearch/adapter/out/BlogKeywordClientAdapter.java
+++ application/src/main/java/kr/pe/karsei/blogsearch/adapter/out/BlogKeywordClientAdapter.java
@@ -8,6 +8,8 @@ import kr.pe.karsei.blogsearch.mapper.BlogKeywordMapper;
 import kr.pe.karsei.blogsearch.port.out.BlogKeywordApiLoadPort;
 import kr.pe.karsei.client.kakao.KakaoBlogApiClient;
 import kr.pe.karsei.client.kakao.dto.KakaoBlogSearch;
+import kr.pe.karsei.client.naver.NaverBlogApiClient;
+import kr.pe.karsei.client.naver.dto.NaverBlogSearch;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
@@ -16,12 +18,12 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class BlogKeywordClientAdapter implements BlogKeywordApiLoadPort {
     private final KakaoBlogApiClient kakaoBlogApiClient;
+    private final NaverBlogApiClient naverBlogApiClient;
 
     @Override
     public FetchBlogKeyword searchBlog(final Pageable pageable, final String query) {
         try {
-            KakaoBlogSearch.Info kakaoBlogSearch = searchBlogWithKakao(pageable, query);
-            return BlogKeywordMapper.mapKakaoBlogSearchToSearchBlogInfo(kakaoBlogSearch);
+            return searchWithKakao(pageable, query);
         }
         catch (FeignException.FeignClientException e) {
             throw new BlogKeywordException(BlogKeywordErrorCode.BAD_REQUEST_API_REQUEST, e);
@@ -34,7 +36,13 @@ public class BlogKeywordClientAdapter implements BlogKeywordApiLoadPort {
         }
     }
 
-    private KakaoBlogSearch.Info searchBlogWithKakao(final Pageable pageable, final String query) {
-        return kakaoBlogApiClient.search(BlogKeywordMapper.mapSearchParamToKakaoBlogSearchParam(pageable, query));
+    private FetchBlogKeyword searchWithKakao(final Pageable pageable, final String query) {
+        KakaoBlogSearch.Info info = kakaoBlogApiClient.search(BlogKeywordMapper.mapSearchParamToKakaoBlogSearchParam(pageable, query));
+        return BlogKeywordMapper.mapKakaoBlogSearchToSearchBlogInfo(info);
+    }
+
+    private FetchBlogKeyword searchWithNaver(final Pageable pageable, final String query) {
+        NaverBlogSearch.Info info = naverBlogApiClient.search(BlogKeywordMapper.mapSearchParamToNaverBlogSearchParam(pageable, query));
+        return BlogKeywordMapper.mapNaverBlogSearchToSearchBlogInfo(info);
     }
 }
--- application/src/main/java/kr/pe/karsei/blogsearch/mapper/BlogKeywordMapper.java
+++ application/src/main/java/kr/pe/karsei/blogsearch/mapper/BlogKeywordMapper.java
@@ -4,10 +4,12 @@ import kr.pe.karsei.blogsearch.adapter.out.BlogKeywordCountJpaEntity;
 import kr.pe.karsei.blogsearch.dto.FetchBlogKeyword;
 import kr.pe.karsei.blogsearch.dto.FetchBlogKeywordTop;
 import kr.pe.karsei.client.kakao.dto.KakaoBlogSearch;
+import kr.pe.karsei.client.naver.dto.NaverBlogSearch;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Pageable;
 
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -43,8 +45,44 @@ public abstract class BlogKeywordMapper {
                     .build());
         }
         FetchBlogKeyword.Meta meta = FetchBlogKeyword.Meta.builder()
-                .pageableCount(kakaoSearchInfo.getMeta().getPageableCount())
                 .totalCount(kakaoSearchInfo.getMeta().getTotalCount())
+                .build();
+        return FetchBlogKeyword.builder()
+                .documents(documents)
+                .meta(meta)
+                .build();
+    }
+
+    public static NaverBlogSearch.Param mapSearchParamToNaverBlogSearchParam(final Pageable pageable, final String query) {
+        return NaverBlogSearch.Param.builder()
+                .query(query)
+                .sort(pageable.getSort()
+                        .stream()
+                        .findFirst()
+                        .map(o -> switch (o.getProperty()) {
+                            case "accuracy" -> NaverBlogSearch.Param.Sort.SIM;
+                            case "recency" -> NaverBlogSearch.Param.Sort.DATE;
+                            default -> NaverBlogSearch.Param.Sort.SIM;
+                        })
+                        .orElse(NaverBlogSearch.Param.Sort.SIM))
+                .start(pageable.getPageNumber())
+                .display(pageable.getPageSize())
+                .build();
+    }
+
+    public static FetchBlogKeyword mapNaverBlogSearchToSearchBlogInfo(final NaverBlogSearch.Info naverSearchInfo) {
+        List<FetchBlogKeyword.Document> documents = new ArrayList<>();
+        for (NaverBlogSearch.Info.Item document : naverSearchInfo.getItems()) {
+            documents.add(FetchBlogKeyword.Document.builder()
+                    .blogName(document.getBloggerName())
+                    .contents(document.getDescription())
+                    .dateTime(document.getDateTime().atStartOfDay(ZoneId.of("Asia/Seoul")))
+                    .title(document.getTitle())
+                    .url(document.getLink())
+                    .build());
+        }
+        FetchBlogKeyword.Meta meta = FetchBlogKeyword.Meta.builder()
+                .totalCount(naverSearchInfo.getTotal())
                 .build();
         return FetchBlogKeyword.builder()
                 .documents(documents)
