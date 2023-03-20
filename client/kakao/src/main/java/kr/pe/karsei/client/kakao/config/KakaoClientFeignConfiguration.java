5f4f8eaf56ef86122d9f515e598e6fcd10a9c72a
Karsei <karseidev@gmail.com>
2023-03-20 22:23:21 +0900

    feat: client timeout 설정

--- client/kakao/src/main/java/kr/pe/karsei/client/kakao/config/KakaoClientFeignConfiguration.java
+++ client/kakao/src/main/java/kr/pe/karsei/client/kakao/config/KakaoClientFeignConfiguration.java
@@ -1,12 +1,23 @@
 package kr.pe.karsei.client.kakao.config;
 
+import feign.Request;
 import feign.RequestInterceptor;
 import kr.pe.karsei.client.kakao.KakaoBlogApiClientProperties;
 import org.springframework.context.annotation.Bean;
 
+import java.util.concurrent.TimeUnit;
+
 public class KakaoClientFeignConfiguration {
+    private final static int CONNECT_TIMEOUT = 5000;
+    private final static int READ_TIMEOUT = 5000;
+
     @Bean
     public RequestInterceptor requestInterceptor(KakaoBlogApiClientProperties properties) {
         return requestTemplate -> requestTemplate.header("Authorization", "KakaoAK " + properties.getApiKey());
+    }
+
+    @Bean
+    public static Request.Options requestOptions() {
+        return new Request.Options(CONNECT_TIMEOUT, TimeUnit.MILLISECONDS, READ_TIMEOUT, TimeUnit.MILLISECONDS, false);
     }
 }
--- client/naver/src/main/java/kr/pe/karsei/client/naver/config/NaverClientFeignConfiguration.java
+++ client/naver/src/main/java/kr/pe/karsei/client/naver/config/NaverClientFeignConfiguration.java
@@ -1,15 +1,26 @@
 package kr.pe.karsei.client.naver.config;
 
+import feign.Request;
 import feign.RequestInterceptor;
 import kr.pe.karsei.client.naver.NaverBlogApiClientProperties;
 import org.springframework.context.annotation.Bean;
 
+import java.util.concurrent.TimeUnit;
+
 public class NaverClientFeignConfiguration {
+    private final static int CONNECT_TIMEOUT = 5000;
+    private final static int READ_TIMEOUT = 5000;
+
     @Bean
     public RequestInterceptor requestInterceptor(NaverBlogApiClientProperties properties) {
         return requestTemplate -> {
             requestTemplate.header("X-Naver-Client-Id", properties.getApiId());
             requestTemplate.header("X-Naver-Client-Secret", properties.getApiSecret());
         };
+    }
+
+    @Bean
+    public static Request.Options requestOptions() {
+        return new Request.Options(CONNECT_TIMEOUT, TimeUnit.MILLISECONDS, READ_TIMEOUT, TimeUnit.MILLISECONDS, false);
     }
 }
