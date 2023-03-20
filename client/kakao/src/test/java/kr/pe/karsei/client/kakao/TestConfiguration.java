6c203918f60adae1c1b6bfcb03798fe211728a84
Karsei <karseidev@gmail.com>
2023-03-20 21:09:02 +0900

    del: 테스트 파일 삭제

--- client/kakao/src/test/java/kr/pe/karsei/client/kakao/TestConfiguration.java
+++
@@ -1,7 +0,0 @@
-package kr.pe.karsei.client.kakao;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class TestConfiguration {
-}
--- client/naver/src/test/java/kr/pe/karsei/client/naver/TestConfiguration.java
+++
@@ -1,7 +0,0 @@
-package kr.pe.karsei.client.naver;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class TestConfiguration {
-}
