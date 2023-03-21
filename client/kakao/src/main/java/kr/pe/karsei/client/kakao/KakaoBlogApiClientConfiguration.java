d4f8ccd52d6cc11602942dbad77e4dd72a51aa27
Karsei <karseidev@gmail.com>
2023-03-21 12:12:47 +0900

    refact: propertysource 추가

--- client/kakao/src/main/java/kr/pe/karsei/client/kakao/KakaoBlogApiClientConfiguration.java
+++ client/kakao/src/main/java/kr/pe/karsei/client/kakao/KakaoBlogApiClientConfiguration.java
@@ -4,8 +4,12 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration(proxyBeanMethods = false)
+@PropertySource(
+        value = { "classpath:application-kakao.properties" }
+)
 @EnableFeignClients
 public class KakaoBlogApiClientConfiguration {
     @Bean
--- client/naver/src/main/java/kr/pe/karsei/client/naver/NaverBlogApiClientConfiguration.java
+++ client/naver/src/main/java/kr/pe/karsei/client/naver/NaverBlogApiClientConfiguration.java
@@ -4,8 +4,12 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration(proxyBeanMethods = false)
+@PropertySource(
+        value = { "classpath:application-naver.properties" }
+)
 @EnableFeignClients
 public class NaverBlogApiClientConfiguration {
     @Bean
